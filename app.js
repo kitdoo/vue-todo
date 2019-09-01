dbcc94106410f121bba6cd8f39f54b40bce2f92e
Ирина Бондаревская <ira.bondarevskaya@gmail.com>
2019-09-01 20:58:38 +0700

    do add and remove

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 new Vue({
     el: "#app",
     data: {
+        currentTask: '',
         tasks: [
             {
                 text: "subscribe",
@@ -15,5 +16,20 @@ new Vue({
                 isCompleted: true
             }
         ]
-    }
+        
+    },
+    methods: {
+            addTask: function() {
+                this.tasks.push({
+                    text: this.currentTask,
+                    isCompleted: false
+                })
+                this.currentTask = '';
+            },
+            removeTask: function(taskText){
+                this.tasks = this.tasks.filter(task =>
+                     {return task.text !==taskText;
+                })
+            }
+        }
 })
