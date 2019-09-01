cfe40c0b0a69a8879d799d661d630c0893281aed
Ирина Бондаревская <ira.bondarevskaya@gmail.com>
2019-09-01 20:36:38 +0700

    do list

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 new Vue({
-    
+    el: "#app",
+    data: {
+        tasks: [
+            {
+                text: "subscribe",
+                isCompleted: false
+            },
+            {
+                text: "walk",
+                isCompleted: true
+            },
+            {
+                text: "learn vue",
+                isCompleted: true
+            }
+        ]
+    }
 })
