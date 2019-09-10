a381bbfc6ef9df63d10ae5880bda9f84825560e1
Ирина Бондаревская <ira.bondarevskaya@gmail.com>
2019-09-10 21:01:00 +0700

    do edit

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ new Vue({
     el: "#app",
     data: {
         currentTask: '',
+        editVaue: '',
         tasks: [
             {
                 text: "subscribe",
-                isCompleted: false
+                isCompleted: false,
+                isEditing:false
             },
             {
                 text: "walk",
-                isCompleted: true
+                isCompleted: true,
+                isEditing:false
             },
             {
                 text: "learn vue",
-                isCompleted: true
+                isCompleted: true,
+                isEditing:false
             }
         ]
         
@@ -24,12 +28,31 @@ new Vue({
                     text: this.currentTask,
                     isCompleted: false
                 })
-                this.currentTask = '';
+                this.currentTask = ''
             },
             removeTask: function(taskText){
                 this.tasks = this.tasks.filter(task =>
-                     {return task.text !==taskText;
+                     {return task.text !==taskText
+                })
+            },
+            changeEditing: function(taskText){
+                this.editValue = taskText
+                this.tasks = this.tasks.map(task=>{
+                    if(task.text === taskText){
+                        task.isEditing = !task.isEditing
+                    }
+                    return task
+                })
+            },
+            editTask: function(taskText){
+                this.tasks = this.tasks.map(task=>{
+                    if(task.text === taskText){
+                        task.isEditing = !task.isEditing
+                        task.text = this.editValue
+                    }
+                    return task
                 })
             }
+
         }
 })
